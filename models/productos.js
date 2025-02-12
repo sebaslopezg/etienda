b5063c060c8f2bf0da32568bacbef276131c1729
elSebas <sebaslg96@gmail.com>
2025-02-12 11:20:38 -0500

    identando estructura en productos.js

--- models/productos.js
+++ models/productos.js
@@ -6,60 +6,60 @@ const {Schema, model, Collection} = require('mongoose')
 
 const productoSchema = Schema({
     nombre: {
-    type: String,
-    required: true,
+        type: String,
+        required: true,
     },
     descripcion: {
-    type: String,
-    default: "",
+        type: String,
+        default: "",
     },
     imagen: {
-    type: String,
-    default: "",
+        type: String,
+        default: "",
     },
     imagenes: [
-    {
-    type: String,
-    },
+        {
+            type: String,
+        },
     ],
     marca: {
-    type: String,
-    default: "",
+        type: String,
+        default: "",
     },
     precio: {
-    type: Number,
-    default: 0,
+        type: Number,
+        default: 0,
     },
     /* categoria: {
     type:
     mongoose.Schema.Types.ObjectId
     ,
-    ref: &quot;Category&quot;,
+    ref: "Category",
     required: true,
     }, */
     existencia: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 255,
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255,
     },
     rating: {
-    type: Number,
-    default: 0,
+        type: Number,
+        default: 0,
     },
     numRevisiones: {
-    type: Number,
-    default: 0,
+        type: Number,
+        default: 0,
     },
     estaOfertado: {
-    type: Boolean,
-    default: false,
+        type: Boolean,
+        default: false,
     },
     fechaCreacion: {
-    type: Date,
-    default: Date.now,
+        type: Date,
+        default: Date.now,
     },
-    },
+},
 {Collection:"producto"}
 )
 
